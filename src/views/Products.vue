3ba998fed0f8a12d75564457f5ff0951bcb8e569
peter.chen1024 <u0052017@gmail.com>
2021-07-12 21:28:24 +0800

    add 產品分類選單

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,64 +1,81 @@
 <template>
   <div class="container">
     <h1>產品列表</h1>
-    <div class="mt-4">
-      <!-- 產品列表 -->
-      <table class="table align-middle">
-        <thead>
-          <tr>
-            <th>圖片</th>
-            <th>商品名稱</th>
-            <th>價格</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in products" :key="item.id">
-            <td style="width: 200px">
-              <div
-                style="height: 100px; background-size: cover; background-position: center"
-                v-if="item.imageUrl"
-                :style="{ backgroundImage: `url(${item.imageUrl})` }"
-              ></div>
-            </td>
-            <td>
-              {{ item.title }}
-            </td>
-            <td>
-              <div class="h5" v-if="!item.price">{{ item.origin_price }} 元</div>
-              <del class="h6" v-if="item.price"
-                >原價 {{ $filters.currency(item.origin_price) }} 元</del
-              >
-              <div class="h5" v-if="item.price">
-                現在只要 {{ $filters.currency(item.price) }} 元
-              </div>
-            </td>
-            <td>
-              <div class="btn-group btn-group-sm">
-                <button
-                  type="button"
-                  class="btn btn-outline-secondary"
-                  @click="pushProductPage(item)"
-                  :disabled="loadingStatus.loadingItem === item.id || !item.is_enabled"
+    <div class="row mt-4">
+      <div class="col-3">
+        <div class="list-group">
+          <a
+            href="#"
+            class="list-group-item list-group-item-action"
+            @click.prevent="selectCategory = ''"
+            >全部商品</a
+          >
+          <a
+            href="#"
+            v-for="item in categories"
+            :key="item"
+            class="list-group-item list-group-item-action"
+            @click.prevent="selectCategory = item"
+            >{{ item }}</a
+          >
+        </div>
+      </div>
+      <div class="col-9">
+        <!-- 產品列表 -->
+        <table class="table align-middle">
+          <thead>
+            <tr>
+              <th>圖片</th>
+              <th>商品名稱</th>
+              <th>價格</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in filterProducts" :key="item.id">
+              <td style="width: 200px">
+                <div
+                  style="height: 100px; background-size: cover; background-position: center"
+                  v-if="item.imageUrl"
+                  :style="{ backgroundImage: `url(${item.imageUrl})` }"
+                ></div>
+              </td>
+              <td>
+                {{ item.title }}
+              </td>
+              <td>
+                <div class="h5" v-if="!item.price">{{ item.origin_price }} 元</div>
+                <del class="h6" v-if="item.price"
+                  >原價 {{ $filters.currency(item.origin_price) }} 元</del
                 >
-                  查看更多
-                </button>
-                <button
-                  type="button"
-                  class="btn btn-outline-danger"
-                  @click="addCart(item.id)"
-                  :disabled="loadingStatus.loadingItem === item.id || !item.is_enabled"
-                >
-                  加到購物車
-                </button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="d-flex justify-content-center">
-      <Pagination :pages="pagination" @get-products="getProducts"></Pagination>
+                <div class="h5" v-if="item.price">
+                  現在只要 {{ $filters.currency(item.price) }} 元
+                </div>
+              </td>
+              <td>
+                <div class="btn-group btn-group-sm">
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary"
+                    @click="pushProductPage(item)"
+                    :disabled="loadingStatus.loadingItem === item.id || !item.is_enabled"
+                  >
+                    查看更多
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-outline-danger"
+                    @click="addCart(item.id)"
+                    :disabled="loadingStatus.loadingItem === item.id || !item.is_enabled"
+                  >
+                    加到購物車
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 
@@ -66,7 +83,6 @@
   <Loading :active="isLoading"></Loading>
 </template>
 <script>
-import Pagination from '@/components/Pagination.vue';
 import emitter from '../methods/eventBus';
 
 export default {
@@ -74,26 +90,24 @@ export default {
     return {
       products: [],
       product: {},
-      pagination: {},
-      currentPage: 1,
       isLoading: false,
       loadingStatus: {
         loadingItem: '',
       },
+      categories: [],
+      selectCategory: '',
     };
   },
-  components: { Pagination },
   methods: {
-    getProducts(page = 1) {
+    getProducts() {
       this.isLoading = true;
-      this.currentPage = page;
-      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products?page=${page}`;
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products/all`;
       this.$http
         .get(url)
         .then((res) => {
           if (res.data.success) {
             this.products = res.data.products;
-            this.pagination = res.data.pagination;
+            this.getCategories();
           } else {
             this.errorAlert(res.data.message);
           }
@@ -102,6 +116,13 @@ export default {
         .catch((err) => {
           console.dir(err);
         });
+    },
+    getCategories() {
+      const categories = new Set();
+      this.products.forEach((item) => {
+        categories.add(item.category);
+      });
+      this.categories = [...categories];
     },
     pushProductPage(item) {
       this.$router.push(`/product/${item.id}`);
@@ -127,6 +148,14 @@ export default {
         .catch((error) => {
           console.dir(error);
         });
+    },
+  },
+  computed: {
+    filterProducts() {
+      if (this.selectCategory === '') {
+        return this.products;
+      }
+      return this.products.filter((item) => item.category === this.selectCategory);
     },
   },
   created() {
