69d6930a49a388f9605e4231c04793c48d0492b6
peter.chen1024 <u0052017@gmail.com>
2021-07-14 23:39:28 +0800

    add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="footer bg-dark text-white py-5">
+    <div class="container d-flex align-items-center justify-content-between">
+      <div class="d-flex flex-column text-white-50">
+        <span>Copyright © 2021 peter.chen1024</span>
+        <span>僅做為個人學習使用，非商業用途</span>
+      </div>
+      <router-link class="ff-Otomanopee h1 text-white text-decoration-none" to="/"
+        >CAMPING</router-link
+      >
+      <div class="link__list d-flex h3">
+        <a
+          href="https://github.com/PeterCHEN-JIANRONG"
+          target="_blank"
+          class="text-white-50 link__icon"
+          ><i class="bi bi-github px-3"></i
+        ></a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100002451825155"
+          target="_blank"
+          class="text-white-50 link__icon"
+          ><i class="bi bi-facebook px-3"></i
+        ></a>
+
+        <router-link to="/" class="text-white-50 link__icon"
+          ><i class="bi bi-instagram px-3"></i
+        ></router-link>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/Front.vue
+++ src/views/Front.vue
@@ -4,15 +4,17 @@
     <ToastMessages></ToastMessages>
   </div>
   <router-view />
+  <Footer></Footer>
 </template>
 <script>
 import emitter from '@/methods/eventBus';
 import ToastMessages from '@/components/ToastMessages.vue';
 import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'Home',
-  components: { ToastMessages, Navbar },
+  components: { ToastMessages, Navbar, Footer },
   provide() {
     return {
       emitter,
