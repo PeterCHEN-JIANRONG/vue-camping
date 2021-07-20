9cfd84743bcba858f05d230ebca68e85ac756f18
peter.chen1024 <u0052017@gmail.com>
2021-07-20 17:56:50 +0800

    add 部落格列表 樣式

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="container">
-    <h1>部落格</h1>
+  <div class="banner"></div>
+  <div class="container p-4">
+    <h1 class="text-center">部落格</h1>
     <div class="row row-cols-1 row-cols-md-2 g-4 mt-3">
       <template v-for="article in articles" :key="article.id">
-        <div class="col" v-if="article.isPublic" @click="$router.push(`/article/${article.id}`)">
+        <div
+          class="col-md-4"
+          v-if="article.isPublic"
+          @click="$router.push(`/article/${article.id}`)"
+        >
           <div class="card">
-            <img :src="article.imageUrl" class="card-img-top" />
+            <div
+              class="card__img border-bottom border-1 border-muted"
+              :style="{ backgroundImage: `url('${article.imageUrl}')` }"
+            ></div>
+            <!-- <img :src="article.imageUrl" class="card-img-top" /> -->
             <div class="card-body">
               <h5 class="card-title">{{ article.title }}</h5>
               <div v-html="article.description"></div>
             </div>
-            <div class="card-footer">
+            <div class="card-footer text-center">
               <router-link
                 :to="`/article/${article.id}`"
                 v-if="article.isPublic"
                 class="btn btn-outline-primary"
               >
-                文章頁面
+                查看文章
               </router-link>
             </div>
           </div>
@@ -54,3 +63,32 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.banner {
+  height: calc(30vh);
+  background-image: url(https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80);
+  background-position: 50% 90%;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.card {
+  &:hover {
+    box-shadow: 0 0.125rem 0.25rem rgba(#000, 0.075);
+  }
+  // .card-title {
+  //   max-width: 100%;
+  //   height: 48px;
+  // }
+  .card__img {
+    width: 100%;
+    height: 250px;
+    background-size: cover;
+    background-position: center;
+    &:hover {
+      background-size: 150%;
+    }
+  }
+}
+</style>
