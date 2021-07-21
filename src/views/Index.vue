a9e950c8962751bc84c1b4e3d5db1971ba1274e0
peter.chen1024 <u0052017@gmail.com>
2021-07-21 17:34:43 +0800

    add 首頁header樣式

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,25 +1,24 @@
 <template>
-  <div class="position-relative">
-    <div
-      class="position-absolute"
-      style="top:0; bottom:0; left:0; right:0; background-image: url(https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80);
-      background-position: center center; opacity: 0.5;"
-    ></div>
-    <div class="container d-flex flex-column" style="min-height: 100vh;">
-      <div class="row justify-content-center my-auto">
-        <div class="col-md-4 text-center">
-          <h2>Lorem ipsum.</h2>
-          <p class="text-muted mb-0">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.
-          </p>
-          <button class="btn btn-dark rounded-0 mt-6">Lorem ipsum.</button>
-        </div>
-      </div>
+  <div class="header position-relative">
+    <div class="header__content position-absolute top-50 start-50 translate-middle text-center">
+      <h1 class="text-white ff-Otomanopee">CAMPING</h1>
+      <p class="text-white h4 mb-3">
+        <!-- 準備好傾聽大自然的蟲鳴鳥叫了嗎？ -->
+        <!-- 受夠了都市的喧囂繁雜嗎？ 找回最原始、初衷的幸福 -->
+        帶您找回人類最原始的幸福
+      </p>
+      <button class="btn btn-primary" @click="$router.push('/products/all')">前往購物</button>
+    </div>
+    <div class="position-absolute bottom-0 start-50 translate-middle-x pb-3">
+      <a href="#" class="h2 text-muted">
+        <i class="bi bi-arrow-down-circle"></i>
+      </a>
     </div>
   </div>
 
   <div class="container">
-    <h1 id="main">首頁</h1>
+    <section id="main"></section>
+    <h1>首頁</h1>
     <p>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea quas nulla nesciunt molestiae quo
       error obcaecati dolores est enim atque, reprehenderit excepturi dolore, velit magni! Nemo
@@ -127,3 +126,19 @@
     </p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: calc(100vh - 84px);
+  background-image: url(https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80);
+  background-size: cover;
+  background-position: center;
+  // opacity: 0.5;
+
+  &__content {
+    background: hsla(0, 0%, 100%, 0.2);
+    padding: 10px 20px;
+  }
+}
+</style>
