fc97736ca41f24cefd83336a737db181b1645928
peter.chen1024 <u0052017@gmail.com>
2021-07-24 00:06:33 +0800

    修正footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,30 +1,35 @@
 <template>
   <footer class="footer bg-dark text-white py-5">
-    <div class="container d-flex align-items-center justify-content-between">
-      <div class="d-flex flex-column text-white-50">
-        <span>Copyright © 2021 peter.chen1024</span>
-        <span>僅做為個人學習使用，非商業用途</span>
-      </div>
-      <router-link class="ff-Otomanopee h1 text-white text-decoration-none" to="/"
-        >CAMPING</router-link
-      >
-      <div class="link__list d-flex h3">
-        <a
-          href="https://github.com/PeterCHEN-JIANRONG"
-          target="_blank"
-          class="text-white-50 link__icon"
-          ><i class="bi bi-github px-3"></i
-        ></a>
-        <a
-          href="https://www.facebook.com/profile.php?id=100002451825155"
-          target="_blank"
-          class="text-white-50 link__icon"
-          ><i class="bi bi-facebook px-3"></i
-        ></a>
-
-        <router-link to="/" class="text-white-50 link__icon"
-          ><i class="bi bi-instagram px-3"></i
-        ></router-link>
+    <div class="container">
+      <div class="row text-white-50 text-center align-items-center">
+        <div class="col-md-4">
+          <p>
+            Copyright © 2021 peter.chen102<br />
+            僅做為個人學習使用，非商業用途
+          </p>
+        </div>
+        <div class="col-md-4">
+          <router-link class="ff-Otomanopee h1 text-white text-decoration-none" to="/"
+            >CAMPING</router-link
+          >
+        </div>
+        <div class="col-md-4 h3 d-flex flex-wrap justify-content-center">
+          <a
+            href="https://github.com/PeterCHEN-JIANRONG"
+            target="_blank"
+            class="text-white-50 link__icon"
+            ><i class="bi bi-github px-3"></i
+          ></a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100002451825155"
+            target="_blank"
+            class="text-white-50 link__icon"
+            ><i class="bi bi-facebook px-3"></i
+          ></a>
+          <router-link to="/" class="text-white-50 link__icon"
+            ><i class="bi bi-instagram px-3"></i
+          ></router-link>
+        </div>
       </div>
     </div>
   </footer>
